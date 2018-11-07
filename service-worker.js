bd13699b5fe7c45c0803eed416cc275f95a1e78d
jacksondeodato <43865699+jacksondeodato@users.noreply.github.com>
2018-11-07 20:49:02 -0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ if (typeof files == 'undefined') {
   files.push('./');
 }
 
-var CACHE_NAME = 'feijao-v1';
+var CACHE_NAME = 'cache-v2';
 
 self.addEventListener('activate', function(event) {
   console.log('[SW] Activate');
